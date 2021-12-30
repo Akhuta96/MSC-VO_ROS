e5565b7eb1a83abd05e6dc805816e98d7a846782
Akhuta <zhangjiahui_2021@163.com>
2021-12-30 23:36:22 +0800

    first commit

---
+++ Examples/ROS/ORB_SLAM2/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/slam/mscvo_ws/src/MSC-VO-main/Examples/ROS/ORB_SLAM2/build/devel:$CMAKE_PREFIX_PATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/slam/mscvo_ws/src/MSC-VO-main/Examples/ROS/ORB_SLAM2/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH='/home/slam/mscvo_ws/src/MSC-VO-main/Examples/ROS/ORB_SLAM2:/home/slam/mscvo_ws/src:/home/slam/catkin_make/src:/opt/ros/melodic/share'
---
+++ Examples/ROS/ORB_SLAM2/build/gtest/googlemock/gtest/CMakeFiles/gtest_main.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gtest_main.dir/src/gtest_main.cc.o"
+  "/home/slam/mscvo_ws/src/MSC-VO-main/Examples/ROS/ORB_SLAM2/lib/libgtest_main.pdb"
+  "/home/slam/mscvo_ws/src/MSC-VO-main/Examples/ROS/ORB_SLAM2/lib/libgtest_main.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gtest_main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
